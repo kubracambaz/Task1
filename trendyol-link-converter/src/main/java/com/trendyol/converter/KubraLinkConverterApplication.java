d20dbb4bf20efc3f944fc380750293b37863c16b
kubracambaz <59199663+kubracambaz@users.noreply.github.com>
2021-09-16 09:41:30 +0300

    demo project
    demo project

---
+++ trendyol-link-converter/src/main/java/com/trendyol/converter/KubraLinkConverterApplication.java
@@ -0,0 +1,13 @@
+package com.trendyol.converter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubraLinkConverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KubraLinkConverterApplication.class, args);
+	}
+
+}
---
+++ trendyol-link-converter/src/test/java/com/trendyol/converter/KubraLinkConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trendyol.converter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KubraLinkConverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
